34f2e6e114ded21ac77ef189b73fd3b477b94d29
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-03-03 09:01:14 -0300

    ejercicio3 finalizado

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -2,3 +2,14 @@
 // salir con “cancelar” deben mostrarse todas las cadenas concatenadas con un
 // guión -.
 
+
+let cadena = prompt('Ingrese una oración');
+
+while(confirm() === true){
+    cadena += ' - '
+    cadena += prompt('Ingrese una oración');
+}
+document.write(`
+<h1>Estas oraciones ingresaste</h1>
+${cadena}
+`)
