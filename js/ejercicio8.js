5ec65ad20e0d56fe05835ca1e52842698e55a1b9
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-03-06 10:59:29 -0300

    empezando ejercicio8

---
+++ js/ejercicio8.js
@@ -0,0 +1,17 @@
+// 8- Crea script para generar pirámide siguiente con los números del 1 al número
+// que indique el usuario (no mayor de 50)
+// 1
+// 12
+// 123
+// 1234
+// 12345
+// 123456
+
+let number = parseInt(prompt("Ingrese un número mayor que uno y menor que 50"));
+
+for(let j = 1; j <= number; j++){
+
+}
+for(let i = 1; i <= number; i++){
+    document.write(j);
+}
