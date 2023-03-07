683337555e5eadd93255aaac5fa62af717cdc330
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-03-07 18:01:37 -0300

    ejercicio13 finalizado

---
+++ js/ejercicio13.js
@@ -0,0 +1,5 @@
+/* 13- Realiza un script que pida un texto y lo muestre en mayúsculas */
+
+let texto = prompt('Ingrese un texto y lo conertiré en mayúsculas =D');
+let mayus = texto.toUpperCase();
+document.write(mayus)
