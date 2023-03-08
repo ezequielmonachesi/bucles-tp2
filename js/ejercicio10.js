b30a74ed9ded10cba8497f1d3106d26d85a6745c
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-03-08 11:07:42 -0300

    ejercicio10 arregladoo

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -3,11 +3,12 @@ Dentro de cada una de las celdas deberá escribirse un número consecutivo en
 orden descendente. Si, por ejemplo, la tabla es de 7×5 los números irán del 35 al 1.
  */
 
-let n1 = parseInt(prompt('Ingrese el número de columnas'));
-let n2 = parseInt(prompt('Ingrese el número de filas'));
+let fila = parseInt(prompt("Ingrese el número de filas"));
+let columna = parseInt(prompt("Ingrese el número de columnas"));
 
-let n = n1*n2;
-document.write(`Columnas y Filas ingresadas ${n1} * ${n2} = Total de celdas: `)
-for(let filas = n; filas >= 1; filas--){
-    document.write(`${filas} `);
+for (let f = 1; f <= columna * fila; f++) {
+  document.write(`${f} `);
+  if (f % columna === 0) {
+    document.write("<br>");
+  }
 }
